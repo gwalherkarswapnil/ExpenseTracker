9ddd464a8ef5772b104da01d9415a2a793feb584
Swapnil <gwalherkarswapnil@gmail.com>
2024-11-08 12:18:04 +0530

    ET-01(WIP) Google partly done

--- ExpenseTracker/ExpenseTracker/ExpenseTrackerApp.swift
+++ ExpenseTracker/ExpenseTracker/ExpenseTrackerApp.swift
@@ -8,8 +8,20 @@
 import SwiftUI
 import SwiftData
 
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
 @main
 struct ExpenseTrackerApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var sharedModelContainer: ModelContainer = {
           let scheme = Schema([
               // entities here
